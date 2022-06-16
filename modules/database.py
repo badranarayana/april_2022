969e0a92eefcbb026e60a1b78f5e0fdd45c75187
badra <abnreddy.a@gmail.com>
2022-06-16 11:11:44 +0530

    HHH

--- modules/database.py
+++ modules/database.py
@@ -94,8 +94,86 @@ virtualenv sqldbenv
 """
 # db connection details
 hostname = '127.0.0.1'
-username = ''
+username = 'badra'
 password = 'Sumanit@123'
 database = 'demodb'
 
-#import MySQLdb
+import MySQLdb
+
+
+# connect with db
+myConnection = MySQLdb.connect(host=hostname, user=username, passwd=password, db=database)
+
+#print(myConnection)
+#cur = myConnection.cursor()
+#
+# # fecth all records
+#cur.execute("SELECT * FROM dept")
+#for dept in cur.fetchall():
+#    print(dept[-1])
+# #print(cur.fetchall())
+
+# lets insert records into db
+
+db_cursor = myConnection.cursor()
+
+sql_statement = """
+INSERT INTO 
+dept(dept_id, department_name, description, location)
+VALUES
+(10,
+'test dept name',
+'test description',
+'hyd');
+"""
+#db_cursor.execute(sql_statement)
+#db_cursor.connection.commit()
+
+
+# update records in db
+update_query = "update dept set department_name='{dept_name}' where dept_id={dept_id}"
+
+query_with_values = update_query.format(dept_name='ABCD', dept_id=10)
+print(query_with_values)
+#db_cursor.execute(query_with_values)
+#db_cursor.connection.commit()
+
+# read
+# insert
+# update
+
+# delete the data
+
+delete_query = "DELETE from dept where dept_id={dept_id}"
+
+del_query = delete_query.format(dept_id=15)
+#db_cursor.execute(del_query)
+#db_cursor.connection.commit()
+#print(db_cursor.rowcount)
+
+
+
+read_query = "SELECT * FROM dept where dept_id={dept_id}"
+r_query = read_query.format(dept_id=2)
+db_cursor.execute(r_query)
+print(db_cursor.fetchall())
+
+
+
+
+
+
+# DDL
+
+sql_employee = """
+CREATE TABLE employee (
+    employee_id INT AUTO_INCREMENT PRIMARY KEY,
+    employee_name VARCHAR(255) NOT NULL,
+    location  VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)  ENGINE=INNODB;
+"""
+
+db_cursor.execute(sql_employee)
+db_cursor.connection.commit()
+
